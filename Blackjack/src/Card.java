82de6bd7f69c50c643927f4de30a5c295e97a4f0
Olivia <otimmermann-27@peddie.org>
2025-02-08 09:41:51 -0500

    base card class

---
+++ Blackjack/src/Card.java
@@ -0,0 +1,34 @@
+public class Card {
+    public static final String FACES[] = {"ZERO", "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
+            "NINE", "TEN", "JACK", "QUEEN", "KING"};
+
+    public static final String SUIT[] = {"Hearts", "Spades", "Diamonds", "Clubs"};
+
+    private String suit;
+    private int face;
+
+    //constructors
+
+
+
+    //modifiers 
+    /* 
+    
+    face = 0;
+    suit = num/13;
+
+    public int getValue()
+    {
+        return face;
+    }
+
+    */
+
+    public boolean equals(Object obj){
+      return false;
+    }
+
+    //toString
+
+    
+}
