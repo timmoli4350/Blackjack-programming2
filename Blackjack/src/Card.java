1ff1b96ad4c79919854347e9ab20e8277f115d61
Olivia <otimmermann-27@peddie.org>
2025-02-08 09:55:12 -0500

    what am i doing

--- Blackjack/src/Card.java
+++ Blackjack/src/Card.java
@@ -1,24 +1,38 @@
+//Olivia Timmermann
+//Programming II (Wolfe)
+//52 cards, blackjack
+
+
 public class Card {
     public static final String FACES[] = {"ZERO", "ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT",
             "NINE", "TEN", "JACK", "QUEEN", "KING"};
 
-    public static final String SUIT[] = {"Hearts", "Spades", "Diamonds", "Clubs"};
+    public static final String SUITS[] = {"Hearts", "Spades", "Diamonds", "Clubs"};
 
     private String suit;
     private int face;
 
     //constructors
+    public BlackJackCard(int face, String suit) { //pass the card, it prints the face and suits
 
+        face = 
+        suit = 
+
+        return
+
+    }
+
+    //default constructor: nothing passed
 
 
     //modifiers 
     /* 
-    
+
     face = 0;
     suit = num/13;
 
-    public int getValue()
-    {
+    //accessors
+    public int getValue(){
         return face;
     }
 
@@ -30,5 +44,13 @@ public class Card {
 
     //toString
 
+    public String toString(){
+
+        return string;
+    }
+
     
 }
+
+
+//edge cases ace and king
